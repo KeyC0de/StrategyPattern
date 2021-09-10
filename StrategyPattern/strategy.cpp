46962265412f1002009053011f2744d65c620dfc
KeyC0de <nik.lazkey@gmail.com>
2021-09-10 14:00:22 +0300

    fixed

---
+++ StrategyPattern/strategy.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include "strategy.h"
+
+
+IQuackBehavior::IQuackBehavior()
+{
+	std::cout << "Quack behavior support\n";
+}
+
+IQuackBehavior::~IQuackBehavior() noexcept
+{
+	std::cout << "Quack behavior terminated\n";
+}
+
+IFlyBehavior::IFlyBehavior()
+{
+	std::cout << "Fly behavior support\n";
+}
+
+IFlyBehavior::~IFlyBehavior() noexcept
+{
+	std::cout << "Fly behavior terminated\n";
+}
+
+IDisplayBehavior::IDisplayBehavior()
+{
+	std::cout << "Display behavior support\n";
+}
+
+IDisplayBehavior::~IDisplayBehavior() noexcept
+{
+	std::cout << "Display behavior terminated\n";
+}
+
+void SimpleFly::fly()
+{
+	std::cout << "Simply fly\n";
+}
+
+void ExtendedFly::fly()
+{
+	std::cout << "Flying for an extended amount of time.\n";
+}
+
+void SimpleQuack::quack()
+{
+	std::cout << "Simply quacking\n";
+}
+
+void NoQuack::quack()
+{
+	std::cout << "This duck does no quack.\n";
+}
+
+void TextDisplay::display()
+{
+	std::cout << "Simple text displaying of the duck.\n";
+}
+
+void GraphicalDisplay::display()
+{
+	std::cout << "Graphical displaying of ducks! The DUCK@!\n";
+}
+
+Duck::Duck( IDisplayBehavior* disp,
+	IQuackBehavior* quack,
+	IFlyBehavior* fly )
+	:
+	m_pDisplay{disp},
+	m_pQuack{quack},
+	m_pFly{fly},
+	m_weight(4)
+{
+	std::cout << "Duck created.\n";
+}
+
+Duck::~Duck()
+{
+	std::cout << "The duck is gone!\n";
+}
+
+Duck::Duck( Duck&& rhs ) noexcept
+	:
+	m_pDisplay{std::move( rhs.m_pDisplay )},
+	m_pQuack{std::move( rhs.m_pQuack )},
+	m_pFly{std::move( rhs.m_pFly )},
+	m_weight(rhs.m_weight)
+{
+
+}
+
+Duck& Duck::operator=( Duck&& rhs ) noexcept
+{
+	std::swap( this->m_pDisplay, rhs.m_pDisplay );
+	std::swap( this->m_pQuack, rhs.m_pQuack );
+	std::swap( this->m_pFly, rhs.m_pFly );
+	this->m_weight = rhs.m_weight;
+	return *this;
+}
+
+void Duck::showcase() const
+{
+	m_pDisplay->display();
+	m_pQuack->quack();
+	m_pFly->fly();
+}
--- StrategyPattern/strategy.h
+++ StrategyPattern/strategy.h
@@ -1,50 +1,33 @@
 #pragma once
 
-#include <iostream>
 #include <memory>
-#include <utility>
 
 
 // family of related algorithms as interfaces
 class IQuackBehavior
 {
 public:
-	IQuackBehavior()
-	{
-		std::cout << "Quack behavior support" << '\n';
-	}
-	virtual ~IQuackBehavior() noexcept
-	{
-		std::cout << "Quack behavior terminated" << '\n';
-	}
+	IQuackBehavior();
+	virtual ~IQuackBehavior() noexcept;
+
 	virtual void quack() = 0;
 };
 
 class IFlyBehavior
 {
 public:
-	IFlyBehavior()
-	{
-		std::cout << "Fly behavior support" << '\n';
-	}
-	virtual ~IFlyBehavior() noexcept
-	{
-		std::cout << "Fly behavior terminated" << '\n';
-	}
+	IFlyBehavior();
+	virtual ~IFlyBehavior() noexcept;
+
 	virtual void fly() = 0;
 };
 
 class IDisplayBehavior
 {
 public:
-	IDisplayBehavior()
-	{
-		std::cout << "Display behavior support" << '\n';
-	}
-	virtual ~IDisplayBehavior() noexcept
-	{
-		std::cout << "Display behavior terminated" << '\n';
-	}
+	IDisplayBehavior();
+	virtual ~IDisplayBehavior() noexcept;
+
 	virtual void display() = 0;
 };
 
@@ -53,110 +36,63 @@ class SimpleFly
 	: public IFlyBehavior
 {
 public:
-	void fly() override
-	{
-		std::cout << "Simply fly\n";
-	}
+	void fly() override;
 };
+
 class ExtendedFly
 	: public IFlyBehavior
 {
 public:
-	void fly() override
-	{
-		std::cout << "Flying for an extended amount of time.\n";
-	}
+	void fly() override;
 };
+
 
 class SimpleQuack
 	: public IQuackBehavior
 {
 public:
-	void quack() override
-	{
-		std::cout << "Simply quacking\n";
-	}
+	void quack() override;
 };
+
 class NoQuack
 	: public IQuackBehavior
 {
 public:
-	void quack() override
-	{
-		std::cout << "This duck does no quack.\n";
-	}
+	void quack() override;
 };
+
 
 class TextDisplay
 	: public IDisplayBehavior
 {
 public: 
-	void display() override
-	{
-		std::cout << "Simple text displaying of the duck.\n";
-	}
+	void display() override;
 };
+
 class GraphicalDisplay
 	: public IDisplayBehavior
 {
 public: 
-	void display() override {
-		std::cout << "Graphical displaying of ducks! The DUCK@!\n";
-	}
+	void display() override;
 };
 
 
 // the "Strategy" object
-// it uses the algorithms it need and can vary them at runtime 
+// it uses the algorithms it needs and can switch between them at runtime 
 class Duck
 {
 	// this object supports the following behavioral algorithms:
-	std::unique_ptr<IDisplayBehavior> m_disp;
-	std::unique_ptr<IQuackBehavior> m_quack;
-	std::unique_ptr<IFlyBehavior> m_fly;
+	std::unique_ptr<IDisplayBehavior> m_pDisplay;
+	std::unique_ptr<IQuackBehavior> m_pQuack;
+	std::unique_ptr<IFlyBehavior> m_pFly;
 	unsigned m_weight;
 public:
 	Duck() = delete;
-	explicit Duck( IDisplayBehavior* disp,
-		IQuackBehavior* quack,
-		IFlyBehavior* fly )
-		:
-		m_disp{disp},
-		m_quack{quack},
-		m_fly{fly},
-		m_weight{4}
-	{
-		std::cout << "Duck created.\n";
-	}
-	~Duck()
-	{
-		std::cout << "The duck is gone!\n";
-	}
+	explicit Duck( IDisplayBehavior* disp, IQuackBehavior* quack, IFlyBehavior* fly );
+	~Duck();
 
-	Duck( Duck&& rhs ) noexcept
-		:
-		m_disp{ std::move( rhs.m_disp ) },
-		m_quack{ std::move( rhs.m_quack ) },
-		m_fly{ std::move( rhs.m_fly ) },
-		m_weight{ rhs.m_weight }
-	{}
-	
-	Duck& operator=( Duck&& rhs ) noexcept
-	{
-		if ( this != &rhs )
-		{
-			std::swap( this->m_disp, rhs.m_disp );
-			std::swap( this->m_quack, rhs.m_quack );
-			std::swap( this->m_fly, rhs.m_fly );
-			this->m_weight = rhs.m_weight;
-		}
-		return *this;
-	}
+	Duck( Duck&& rhs ) noexcept;
+	Duck& operator=( Duck&& rhs ) noexcept;
 
-	void showcase() const
-	{
-		m_disp->display();
-		m_quack->quack();
-		m_fly->fly();
-	}
+	void showcase() const;
 };
